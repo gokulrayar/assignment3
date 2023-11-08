6c8e22f97910c29d9eb0a7eda5c8402257a88fc9
gokulrayar <gokulrayar>
2023-11-08 17:10:11 +0530

    assignment3

---
+++ assaignment 3/assaignment 3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assaignment_3 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // using tryparse
+            string numberString = "123";
+            int number;
+            if (int.TryParse(numberString,out number))
+            {
+                Console.WriteLine("conversion successful. Converted vlue:" + number);
+            }
+            else
+            {
+                Console.WriteLine("conversion faiked.Invalid input.");
+            }
+            //using convert
+            string numberString = "3.14";
+            double number;
+
+            try
+            {
+                number = Convert.ToDouble(numberString);
+                Console.WriteLine("Conversion successful. Converted value: " + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Conversion failed. Input value is too large or too small.");
+            }
+            */
+
+            //using parse
+
+            string numberString = "3.12334";
+            decimal number;
+
+            try
+            {
+                number = decimal.Parse(numberString);
+                Console.WriteLine("Conversion successful. Converted value: " + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion failed. Invalid input format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Conversion failed. Input value is too large or too small.");
+            }
+        }
+    }
+}
---
+++ assignment 3 p2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_3_p2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using TryParse to convert a string to an integer and then multiply by 5
+            Console.Write("Enter an integer: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                int multipliedResult = result * 5;
+                Console.WriteLine($"Converted and multiplied result: {multipliedResult}");
+            }
+            else
+            {
+                Console.WriteLine("Conversion to integer failed. Invalid input.");
+            }
+
+            // Using Convert to convert a string to DateTime and subtract 1 month
+            Console.Write("Enter a date (MM/dd/yyyy): ");
+            string dateInput = Console.ReadLine();
+            try
+            {
+                DateTime dateResult = Convert.ToDateTime(dateInput);
+                dateResult = dateResult.AddMonths(-1);
+                Console.WriteLine($"Converted date with 1 month subtracted: {dateResult:MM/dd/yyyy}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion to DateTime failed. Invalid input.");
+            }
+
+            // Using Parse to convert a string to TimeSpan and add 2 hours
+            Console.Write("Enter a time (hh:mm:ss): ");
+            string timeInput = Console.ReadLine();
+            try
+            {
+                TimeSpan timeResult = TimeSpan.Parse(timeInput);
+                timeResult = timeResult.Add(new TimeSpan(2, 0, 0));
+                Console.WriteLine($"Converted time with 2 hours added: {timeResult:hh\\:mm\\:ss}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion to TimeSpan failed. Invalid input.");
+            }
+        }
+    }
+}
+
